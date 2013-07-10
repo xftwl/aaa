423d2485e3aaaf2c632d07b9854152570cdf6f91
Your Name <you@example.com>
2013-07-10 17:48:05 +0900

    retry

--- public/auth.php
+++ public/auth.php
@@ -1,20 +1,27 @@
-﻿<?php
+<?php
 
+// Found in the 'helper-libs' folder, or download twilio-php from http://twilio.com/docs/libraries
 require "Services/Twilio/Capability.php";
 
-$accountSid = "AC4321cb32c16fdee55f038b630b763650";
+$accountSid = "AC4321cb32c16fdee55f038b630b763650"; 
 $authToken = "7bc4854b9b93ef90fe525f6426294ce8";
 
-$appSid = "APdf33abf7bd42360d00c2153a9f6b655d";
+// The app outgoing connections will use:
+$appSid = "APdf33abf7bd42360d00c2153a9f6b655d"; 
 
-$clientName = $_REQUEST["clientName"];
+// The client name for incoming connections:
+$clientName = "monkey";
 
 $capability = new Services_Twilio_Capability($accountSid, $authToken);
 
+// This would allow incoming connections as $clientName: 
 $capability->allowClientIncoming($clientName);
 
+// This allows outgoing connections to $appSid with the "From" parameter being $clientName 
 $capability->allowClientOutgoing($appSid, array(), $clientName);
 
+// This would return a token to use with Twilio based on 
+// the account and capabilities defined above 
 $token = $capability->generateToken();
 
 echo $token;
