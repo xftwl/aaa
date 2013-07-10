7c4c2540c9b29c8f0b60c69ffab0ea4aad6987ed
Your Name <you@example.com>
2013-07-10 10:39:11 +0900

    6

---
+++ public/auth.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+require "Services/Twilio/Capability.php";
+
+$accountSid = "AC4321cb32c16fdee55f038b630b763650";
+$authToken = "7bc4854b9b93ef90fe525f6426294ce8";
+
+$appSid = "APdf33abf7bd42360d00c2153a9f6b655d";
+
+$clientName = $_REQUEST["clientName"];
+
+$capability = new Services_Twilio_Capability($accountSid, $authToken);
+
+$capability->allowClientIncoming($clientName);
+
+$capability->allocClientOutgoing($appSid, array(), $clientName);
+
+$token = $capaility->generateToken();
+
+echo $token;
+?>
