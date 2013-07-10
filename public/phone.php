995f7fe4456afd72013177de158e307bf9250267
Your Name <you@example.com>
2013-07-10 10:05:22 +0900

    5

---
+++ public/phone.php
@@ -0,0 +1,12 @@
+﻿<?php
+header("content-type: text/xml");
+
+$number = $_REQUEST["PhoneNumber"];
+
+$callerId = "+815031597071";
+?>
+<Response>
+    <Dial callerId="<?php echo $callerId ?>">
+        <Number><?php echo $number;?></Number>
+    </Dial>
+</Response>
