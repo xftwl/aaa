b6c34d604ed2b2893cf86bf6a4866034a8f27bdb
Your Name <you@example.com>
2013-07-10 18:14:51 +0900

    incom

--- public/reply.php
+++ public/reply.php
@@ -3,5 +3,7 @@ header("content-type: text/xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-<Say>Hello Hello</Say>
+    <Dial>
+        <Client>monkey</Client>
+    </Dial>
 </Response>
